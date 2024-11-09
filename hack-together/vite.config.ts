f266965c67a14c7c3e8b4b109b704e66b09aa15c
Francisco Freitas <fj.freitas@campus.fct.unl.pt>
2024-11-09 13:58:34 +0000

    shadcn imported

--- hack-together/vite.config.ts
+++ hack-together/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -8,5 +9,10 @@ export default defineConfig({
     ViteYaml(),
     react(),
     tsconfigPaths(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
